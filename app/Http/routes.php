42bd9945eb67c211fdd805df1eb6c35f15f4eb44
yeisonsa <yeison.ysa@gmail.com>
2015-11-24 13:41:50 -0500

    Probando rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,23 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', function (){
+	return view('welcome');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', function (){
+	return view('prueba');
+});
+
+Route::get('/otro', function (){
+	return view('otro');
 });
 
 
-Route::resource('/news', 'AdminController');
-
-Route::get('{id}', function($id){
-	return 'Esta Url no existe...';
-});
---
+++ resources/views/otro.blade.php
@@ -0,0 +1,7 @@
+@extends('prueba')
+
+@section('sex')
+
+<h2> Este ya es otro articulo que hereda el estio de prueba </h2>
+
+@stop
